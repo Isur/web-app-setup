95952e9b5aeed2bc4dab42c098ca6202abd938be
Isur <bednarczykartur96@gmail.com>
2024-01-09 22:57:09 +0100

    Correct proxy_pass, example front changes, example api changes

--- hello-world-front/src/comps/component.tsx
+++ hello-world-front/src/comps/component.tsx
@@ -20,7 +20,7 @@ export default function Component() {
 
   return (
     <div>
-      <button onClick={onClick}>Click me</button>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={onClick}>Click me</button>
       <p>{data}</p>
     </div>
   );
